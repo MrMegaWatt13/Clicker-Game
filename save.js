4122e9115418e7ddcee52eadacdfeb98d35fd6ac
MrMegaWatt13 <112874353+MrMegaWatt13@users.noreply.github.com>
2022-09-21 19:43:13 +0800

    v1.0.2

--- save.js
+++ save.js
@@ -2,7 +2,8 @@ var gameData = {
   click: 0,
   autoclick: 0,
   autoclickII: 0,
-  
+  autoclickCost: 10,
+  autoclickIICost: 150,
 }
 var savegame = JSON.parse(localStorage.getItem("Clicker GameSave"))
 if (savegame !== null) {
