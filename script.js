70280e9044926626785d2efe755782fbbf09bf82
MrMegaWatt13 <112874353+MrMegaWatt13@users.noreply.github.com>
2022-09-21 18:02:09 +0800

    v1.0.0

---
+++ script.js
@@ -0,0 +1,52 @@
+var click = 0;
+
+function clicksClick(number){
+    click = click + number;
+    document.getElementById("click").innerHTML = click;
+};
+
+var autoclicker = 0;
+
+function buyAutoClicker(){
+    var autoclickerCost= Math.floor(10 * Math.pow(1.3,autoclicker));        
+    if(click >= autoclickerCost){                                                                  
+        autoclicker = autoclicker + 1;                                                                       
+        click = click - autoclickerCost,                                                         
+        document.getElementById('autoclicker').innerHTML = autoclicker;    
+        document.getElementById('click').innerHTML = click;            
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.3,autoclicker));       
+    document.getElementById('autoclickerCost').innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+    
+    clicksClick(autoclicker);
+    
+}, 1000);
+
+var autoclickerII = 0;
+
+function buyAutoClickerII(){
+    var autoclickerIICost= Math.floor(150 * Math.pow(1.4,autoclickerII));        
+    if(click >= autoclickerIICost){                                                                  
+        autoclickerII = autoclickerII + 1;                                                                       
+        click = click - autoclickerIICost,                                                         
+        document.getElementById('autoclickerII').innerHTML = autoclickerII;    
+        document.getElementById('click').innerHTML = score;            
+    };
+    var nextCost = Math.floor(150 * Math.pow(1.4,autoclickerII));       
+    document.getElementById('autoclickerIICost').innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+    
+    clicksClick(autoclickerII);
+    
+}, 200);
+
+var prestigepoint = 0
+function prestigepointClick(number){
+     prestigepoint = prestigepoint + number;
+    document.getElementById("prestigepoint").innerHTML = prestigepoint;
+};
