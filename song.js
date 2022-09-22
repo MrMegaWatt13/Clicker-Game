2f673b0ee1ec716c65bdf26ea22dd845ff1f962a
MrMegaWatt13 <112874353+MrMegaWatt13@users.noreply.github.com>
2022-09-22 20:43:13 +0800

    1.2.5

---
+++ song.js
@@ -0,0 +1,16 @@
+document.getElementById("soundTag").innerHTML = "<audio controls volume preload='none' src='Sunburst.mp3'></audio>";
+
+/*$('#button_play').on('click', function() {
+
+	$("audio")[0].play();
+  
+  $('#button_pause').show();
+  $('#button_play').hide();
+});
+$('#button_pause').on('click', function() {
+	
+	$("audio")[0].pause();
+  
+  $('#button_play').show();
+  $('#button_pause').hide();
+});*/
